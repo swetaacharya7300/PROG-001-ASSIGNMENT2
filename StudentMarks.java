b0128a5b4d6be71d188f108328dc0b277b6051e5
swetaacharya7300 <140718155+swetaacharya7300@users.noreply.github.com>
2023-10-01 15:37:07 +1100

    Created students class

---
+++ StudentMarks.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class StudentMarks here.
+ *
+ * @author Sweta Acharya
+ * @version 10/01/2023
+ */
+import java.io.*;
+import java.util.Arrays;
+
+class Student {
+    String lastName;
+    String firstName;
+    int studentID;
+    double a1Mark;
+    double a2Mark;
+    double a3Mark;
+    double totalMark;
+
+    public Student(String lastName, String firstName, String studentID, double a1Mark, double a2Mark, double a3Mark) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.studentID = studentID;
+        this.a1Mark = a1Mark;
+        this.a2Mark = a2Mark;
+        this.a3Mark = a3Mark;
+
+    }
+
+
+}
+
+public class StudentMarks {
+    public static void main(String[] args) {
+
+    }
+
+
+}
+

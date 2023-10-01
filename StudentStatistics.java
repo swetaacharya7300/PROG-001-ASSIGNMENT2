3be10d753df2650cc076d9cea891c8287b4d843a
swetaacharya7300 <140718155+swetaacharya7300@users.noreply.github.com>
2023-10-01 16:03:06 +1100

    Function2
    Calculates the total mark for each student from the assessment marks and print out the list of students with their name, student ID, assessment
    marks and the total mark.

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class StudentMarks here.
+ * Total Marks.
  *
  * @author Sweta Acharya
  * @version 10/01/2023
@@ -10,16 +10,12 @@ import java.util.*;
 
 class Student {
 
-    //Variable Declaration
-    String lastName;
-    String firstName;
-    String studentID;
-    double ass1Mark;
-    double ass2Mark;
-    double ass3Mark;
+    double a3Mark;
     double totalMark;
 
     public Student(String lastName, String firstName, String studentID, double ass1Mark, double ass2Mark, double ass3Mark) {
+    public Student(String lastName, String firstName, 
+    String studentID, double ass1Mark, double ass2Mark, double ass3Mark) {
         this.lastName = lastName;
         this.firstName = firstName;
         this.studentID = studentID;
@@ -27,75 +23,42 @@ class Student {
         this.ass2Mark = ass2Mark;
         this.ass3Mark = ass3Mark;
 
+        calculateTotalMark();
+
     }
 
 
 }
-public class StudentStatistics
-{
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        //allows user to enter the File Name
-        System.out.print("Please Enter the file name with Extension: ");
-        String fileName = scanner.nextLine();
-
-        try {
-            BufferedReader buff = new BufferedReader(new FileReader(fileName));
-
-            String textLine;
-
-            while ((textLine = buff.readLine()) != null) {
-                System.out.println(textLine);
-            }
-
-            buff.close();
-        } catch (IOException e) {
-            System.err.println("Error reading student data from the file.");
-        }
-
-        scanner.close();
+    private void calculateTotalMark() {
+        totalMark = ass1Mark + ass2Mark + ass3Mark;
     }
 
-    private static Student[] readStudentDataFromFile(String fileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            int lineNumber = 0;
-            Student[] std = new Student[70]; 
-
-            while ((line = reader.readLine()) != null) {
-                lineNumber++;
-                if (lineNumber == 1) {
-                    //Skipping first line
-                    continue;
-                }
-
-                String[] segment = line.split(",");
-                if (segment.length >= 5) { 
-                    // Check for at least 5 columns for three assignment marks
-                    String lastName = segment[0].trim();
-                    String firstName = segment[1].trim();
-                    String studentID = segment[2].trim();
-                    double ass1Mark = Double.parseDouble(segment[3]);
-                    double ass2Mark = Double.parseDouble(segment[4]);
-                    double ass3Mark = 0.0; 
-                    // Default value for assg 3 if missing
-
-                    if (segment.length >= 6) { 
-                        // Checking Assg3 if present
-                        ass3Mark = Double.parseDouble(segment[5]);
-                    }
-
-                    std[lineNumber - 2] = new Student(lastName, firstName, studentID, ass1Mark, ass2Mark, ass3Mark);
-                }
-            }
-            return std;
-        } catch (IOException e) {
+}
+public class StudentStatistics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+@@ -72,6 +76,23 @@ private static Student[] readStudentDataFromFile(String fileName) {
             e.printStackTrace();
             return null;
         }
+
     }
+
+    private static void calculateTotalMarksAndPrint(Student[] students) {
+        System.out.println("List of Students with Assessment Marks and Total Marks:");
+        for (Student student : students) {
+            if (student != null) { // Check if the student is not null
+                double totalMark = student.ass1Mark + student.ass2Mark + student.ass3Mark;
+                System.out.println("Name: " + student.firstName + " " + student.lastName +
+                    ", Student ID: " + student.studentID +
+                    ", ASS1 Mark: " + student.ass1Mark +
+                    ", ASS2 Mark: " + student.ass2Mark +
+                    ", ASS3 Mark: " + student.ass3Mark +
+                    ", Total Mark: " + totalMark);
+            }
+        }
+    }
+
 
 }
 
